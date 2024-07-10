bfd28f2a0a4db1fcceb49b528e88f850db50243f
Narendra Kumar <narendrasingh909@gmail.com>
2024-07-10 13:21:16 +0530

    Routing added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule } from '@angular/router';
+
+// Need to import RouterModule for routing
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { AboutComponent } from './components/about/about.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:HomeComponent},
+    {path:'home',component:HomeComponent},
+    {path:'contact',component:ContactComponent},
+    {path:'about',component:AboutComponent},
+    {path:'posts',component:PostsComponent},
+    {path:'**',component:PagenotfoundComponent}
+];

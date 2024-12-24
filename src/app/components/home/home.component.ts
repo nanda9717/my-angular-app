03d1eec3cd370cc4479e2ca604b2cc85f55f2199
Narendra Kumar <narendrasingh909@gmail.com>
2024-12-24 09:12:56 +0530

    Home component changes

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+ title = "Angular works"
+
+ constructor(){
+  console.log("constructor")
+ }
+
+ ngOnInit(){
+  console.log('====ngOnInit====')
+ }
+
+ ngDoCheck(){
+  console.log("====ngDoCheck===")
+ }
+
+ ngAfterContentInit(){
+  console.log("==== After content Init===")
+ }
+
+ ngOnDestroy(){
+  console.log("Destroy")
+ }
 
 }
